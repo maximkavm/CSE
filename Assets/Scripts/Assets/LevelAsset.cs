7d660c5bbc7956be6184c8ca10d9b4895e9b4f50
Maxim Meschankin <maxikavm@gmail.com>
2021-10-21 17:59:17 +0500

    21_10_21

---
+++ Assets/Scripts/Assets/LevelAsset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets{
+[CreateAssetMenu(fileName = "Assets/LevelAsset")]
+public class LevelAsset : ScriptableObject
+
+{
+    
+    
+}
+
+}
