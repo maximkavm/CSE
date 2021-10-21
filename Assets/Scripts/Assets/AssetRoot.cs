b6debe7c508f9a1cf31269895fd89d95075dd2cc
Maxim Meschankin <maxikavm@gmail.com>
2021-10-21 17:58:41 +0500

    21_10_2021

---
+++ Assets/Scripts/Assets/AssetRoot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets{
+//[MenuItem("Asset root")]
+[CreateAssetMenu(fileName = "Assets/AssetRoot")]
+public class AssetRoot : ScriptableObject
+{
+
+}
+
+}
